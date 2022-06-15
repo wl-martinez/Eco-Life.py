d2de0c0e94449218bf4d74106354ffb6bfe06ee8
wl-martinez <2wl.martinez@duocuc.clgit config --global user.email wl.martinez@duocuc.cl>
2022-06-15 03:38:01 -0400

    “avance”

--- app/views.py
+++ app/views.py
@@ -12,11 +12,23 @@ def productos(request):
     return render(request, 'app/productos.html', data)
 
 def carrito(request):
+    carrito = Producto.objects.all()
+    data = {
+         'carrito': carrito
+    }
     return render(request, 'app/carrito.html')
 
 def ingresar(request):
+    ingresar = Producto.objects.all()
+    data = {
+         'ingresar': ingresar
+    }
     return render(request, 'app/ingresar.html')
 
 def index(request):
+    index = Producto.objects.all()
+    data = {
+         'index': index
+    }
     return render(request, 'app/index.html')
 
